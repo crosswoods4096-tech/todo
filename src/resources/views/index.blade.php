8d4923b69268416eb5817da226018db6ea618992
takepon <crosswoods4096@yahoo.co.jp>
2025-12-20 17:54:49 +0900

    追加機能実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -12,7 +12,8 @@
 </div>
 
 <div class="todo__content">
-  <form class="create-form">
+  <form class="create-form" action="/todos" method="post">
+   @csrf
     <div class="create-form__item">
       <input class="create-form__item-input" type="text" name="content">
     </div>
@@ -22,6 +23,7 @@
   </form>
   <div class="todo-table">
     <table class="todo-table__inner">
+      @foreach ($todos as $todo)
       <tr class="todo-table__row">
         <th class="todo-table__header">Todo</th>
       </tr>
@@ -29,7 +31,7 @@
         <td class="todo-table__item">
           <form class="update-form">
             <div class="update-form__item">
-              <input class="update-form__item-input" type="text" name="content" value="test">
+              <input class="update-form__item-input">{{ $todo['content'] }}</input>type="text" name="content" value="test">
             </div>
             <div class="update-form__button">
               <button class="update-form__button-submit" type="submit">更新</button>
@@ -44,25 +46,8 @@
           </form>
         </td>
       </tr>
-      <tr class="todo-table__row">
-        <td class="todo-table__item">
-          <form class="update-form">
-            <div class="update-form__item">
-              <input class="update-form__item-input" type="text" name="content" value="test2">
-            </div>
-            <div class="update-form__button">
-              <button class="update-form__button-submit" type="submit">更新</button>
-            </div>
-          </form>
-        </td>
-        <td class="todo-table__item">
-          <form class="delete-form">
-            <div class="delete-form__button">
-              <button class="delete-form__button-submit" type="submit">削除</button>
-            </div>
-          </form>
-        </td>
-      </tr>
+      @endforeach
+
     </table>
   </div>
 </div>
